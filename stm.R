286567f38915e6962173502039b9bfc19183e6da
Zaboretzky Bendegúz <91075776+WhisperPotato@users.noreply.github.com>
2021-11-14 13:04:00 +0100

    Add files via upload

--- stm.R
+++ stm.R
@@ -1,73 +1,59 @@
+# szükséges csomagok betöltése
 install.packages("stm")
 
 library(stm)        # Package for sturctural topic modeling
 library(igraph)     # Package for network analysis and visualisation
 library(stmCorrViz) # Package for hierarchical correlation view of STMs
 
+#adatbázis beolvasás és szükséges változók létrehozása
 data <- read.csv("C:/Users/zabor/Desktop/STM/final_stm.csv") 
 processed <- textProcessor(data$txts_contwrd, metadata=data, lowercase = FALSE, removestopwords = FALSE, removenumbers = FALSE, removepunctuation = FALSE, stem = FALSE)
 
 out <- prepDocuments(processed$documents, processed$vocab, processed$meta)
 
 meta <- out$meta
-
-kResult <- searchK(out$documents, out$vocab, K=c(5:19), prevalence=~day_num, data=meta)
+# optimális topikszám keresés - koherencia érték vizsgálat
+kResult <- searchK(out$documents, out$vocab, K=c(5:19), data=meta)
 plot(kResult)
 
-poliblogPrevFit <- stm(out$documents, out$vocab, K=19, max.em.its=10, data=meta, prevalence=~day_num, init.type="Spectral", 
+poliblogPrevFit <- stm(out$documents, out$vocab, K=19, max.em.its=10, data=meta, init.type="Spectral", 
                        seed=84)
+
+# létrejött topikok vizualizációja
 plot(poliblogPrevFit, type="summary", xlim=c(0,.4))
 
-plot(poliblogPrevFit, type="labels", topics=c(9,3,12))
+plot(poliblogPrevFit, type="labels")
 
-#storage <- manyTopics(out$documents, out$vocab, K=c(5:20), data=meta$onlyday, runs=5)
-
-#model <- selectModel(out$documents, out$vocab, K=19, data=meta$onlyday, runs=5)
-
-#plotModels(model)
-
-#meta$onlyday <- as.factor(out$meta$onlyday)
 prep <- estimateEffect(5:19 ~ day_num, poliblogPrevFit, meta=out$meta, 
                        uncertainty="Global")
 
-###   test_STM.png kódja   ###
-plot(prep, "day_num", method="continuous", topics=19, model=z, printlegend=FALSE, xaxt="n", 
-     xlab="Time (2020)")
-monthseq <- seq(from=as.Date("2020-03-11"), to=as.Date("2020-12-31"), by="month")
-monthnames <- months(monthseq)
-axis(1, at=as.numeric(monthseq)-min(as.numeric(monthseq)), labels=monthnames)
-###   test_STM.png kódja   ###
-
+# korreláció vizsgálat
 mod.out.corr <- topicCorr(poliblogPrevFit)
 plot(mod.out.corr)
 
-##################################################################################
+##########################    idő (day_num) meta-változó bevonása     #########################################
 kResult2 <- searchK(out$documents, out$vocab, K=c(5:19), prevalence=~s(day_num), data=meta)
 plot(kResult2)
 
 poliblogPrevFit2 <- stm(out$documents, out$vocab, K=19, max.em.its=10, data=meta, prevalence=~s(day_num), init.type="Spectral", 
                        seed=84)
+
+# létrejött topikok vizualizációja
 plot(poliblogPrevFit2, type="summary", xlim=c(0,.4))
 
 plot(poliblogPrevFit2, type="labels")
 
-#storage <- manyTopics(out$documents, out$vocab, K=c(5:20), data=meta$onlyday, runs=5)
-
-#model <- selectModel(out$documents, out$vocab, K=19, data=meta$onlyday, runs=5)
-
-#plotModels(model)
-
-#meta$onlyday <- as.factor(out$meta$onlyday)
 prep2 <- estimateEffect(5:19 ~ s(day_num), poliblogPrevFit2, meta=out$meta, 
                        uncertainty="Global")
 
-###   test_STM.png kódja   ###
+# topic proportions
 plot(prep2, "day_num", method="continuous", topics=19, model=z, printlegend=FALSE, xaxt="n", 
-     xlab="Time (2020)")
+     xlab="Time (2020.03.11 - 2020.12.31.)")
 monthseq <- seq(from=as.Date("2020-03-11"), to=as.Date("2020-12-31"), by="day")
 monthnames <- months(monthseq)  ##ide vmi kéne, hogy naponta legyen
 axis(1, at=as.numeric(monthseq)-min(as.numeric(monthseq)), labels=monthnames)
-###   test_STM.png kódja   ###
+# topic proportions
 
+# korreláció vizsgálat
 mod.out.corr2 <- topicCorr(poliblogPrevFit2)
 plot(mod.out.corr2)
